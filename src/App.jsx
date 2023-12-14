dc6e9bba0f8c1240ce978adccfb9e0013ff12194
Safia Messaoud <81869764+safiames@users.noreply.github.com>
2023-12-14 13:04:24 +0200

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,18 @@ const App = () => {
   const courses = [
     {
       name: "Half Stack application development",
-      id: 1,
       parts: [
         {
           name: "Fundamentals of React",
           exercises: 10,
-          id: 1,
         },
         {
           name: "Using props to pass data",
           exercises: 7,
-          id: 2,
         },
         {
           name: "State of a component",
           exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
         },
       ],
     },
@@ -45,13 +36,15 @@ const App = () => {
       ],
     },
   ];
+
   return (
-    <>
+    <div>
       <h1>Web development curriculum</h1>
       {courses.map((course) => (
-        <Course key={course.id} kurssi={course} />
+        <Course course={course} />
       ))}
-    </>
+    </div>
   );
 };
+
 export default App;
--- src/Course.jsx
+++ src/Course.jsx
@@ -1,50 +1,40 @@
-const Header = ({ header }) => {
-  return <h2> {header} </h2>;
-};
-const Part = (props) => {
-  console.log("propsin osat:", props);
-
-  return (
-    <>
-      <p>
-        {props.pala.name} {props.pala.exercises}
-      </p>
-    </>
-  );
+const Header = (props) => {
+  return <h2>{props.course.name}</h2>;
 };
 
 const Content = (props) => {
-  console.log("sisalto:", props.sisalto);
+  const courseParts = props.course.parts;
 
-  return (
-    <>
-      {props.sisalto.map((osa, tunniste) => (
-        <Part key={tunniste} pala={osa} />
-      ))}
-    </>
-  );
+  return courseParts.map((part) => (
+    <Part name={part.name} exercises={part.exercises} key={part.name} />
+  ));
 };
 
-const Course = (props) => {
-  console.log(props);
+const Part = (props) => {
   return (
-    <>
-      <Header header={props.kurssi.name} />
-      <Content sisalto={props.kurssi.parts} />
-      <Total sum={props.kurssi.parts} />
-    </>
+    <p>
+      {props.name} {props.exercises}
+    </p>
   );
 };
 
 const Total = (props) => {
-  const summa = props.sum.reduce(
-    (sum, lukumaara) => sum + lukumaara.exercises,
-    0
-  );
+  const parts = props.course.parts;
+  const sum = parts.reduce((total, part) => total + part.exercises, 0);
   return (
-    <>
-      <strong>total of </strong> {summa} <strong>exercises </strong>
-    </>
+    <p>
+      <strong>Total of {sum} exercises</strong>
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
   );
 };
 
